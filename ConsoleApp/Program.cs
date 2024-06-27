bd5ea09935f11e9435ee4fc95dae989380029d75
tota <kerelusmoner@gmail.com>
2024-06-27 13:23:42 +0300

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int[] game = new int[5] { 1, 2, 3, 4, 5 };
+            int s = 0;
+            
+            foreach ( int i in game) 
+            {
+                Console.WriteLine(i);
+                s=s+i;
+                
+            }
+        }
+    }
+}
